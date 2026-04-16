288d1541c4816a670bec0172793bf8926d5c029c
datosfresia-stack <datosfresia@gmail.com>
2026-04-15 23:54:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("¡ESTOY VIVO!")
-st.write("Miguel, si ves esto, la web ya funciona. Ahora solo falta la IA.")
+st.write("Miguel, si ves esto, la web ya funciona. Ahora solo falta Cambiar Genini por una IA que funcione.")
